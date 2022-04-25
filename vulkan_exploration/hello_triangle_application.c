15d033c204c72fbe9735e131d05e773facb4a2f7
chopin9938 <tkowalski9938@gmail.com>
2022-04-25 11:30:38 -0400

    created window with glfw

--- vulkan_exploration/hello_triangle_application.c
+++ vulkan_exploration/hello_triangle_application.c
@@ -1,19 +1,52 @@
 #include "hello_triangle_application.h"
 
-static void init_vulkan(void) {
+// Activates GLFW preprocessor directive to also load Vulkan header
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdlib.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+static GLFWwindow* initWindow(void) {
+    // initializes the GLFW library
+    glfwInit();
+    
+    // tells GLFW not to create an OpenGL context
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    
+    // disables resizing windows
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    
+    // create the window
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", NULL, NULL);
+    
+    return window;
+}
+
+static void initVulkan(void) {
     
 }
 
-static void main_loop(void) {
-    
+static void mainLoop(GLFWwindow *window) {
+    // keeps the application running until window closed
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
 }
 
-static void cleanup(void) {
+static void cleanup(GLFWwindow *window) {
+    // deallocates window memory
+    glfwDestroyWindow(window);
     
+    // terminates glfw
+    glfwTerminate();
 }
 
 void run(void) {
-    init_vulkan();
-    main_loop();
-    cleanup();
+    GLFWwindow* window = initWindow();
+    initVulkan();
+    mainLoop(window);
+    cleanup(window);
 }
--- vulkan_exploration/main.c
+++ vulkan_exploration/main.c
@@ -4,5 +4,6 @@
 
 int main(void) {
     fprintf(stderr, "hello world");
+    run();
     return 0;
 }
