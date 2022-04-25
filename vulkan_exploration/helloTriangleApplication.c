bb8b81862c696246ea75c43ca5e4b2c825d09a28
chopin9938 <tkowalski9938@gmail.com>
2022-04-25 13:55:46 -0400

    created instance

--- vulkan_exploration/helloTriangleApplication.c
+++ vulkan_exploration/helloTriangleApplication.c
@@ -6,8 +6,12 @@
 
 #include <stdlib.h>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+static const uint32_t WIDTH = 800;
+static const uint32_t HEIGHT = 600;
+
+static VkInstance instance;
+
+
 
 static GLFWwindow* initWindow(void) {
     // initializes the GLFW library
@@ -25,9 +29,52 @@ static GLFWwindow* initWindow(void) {
     return window;
 }
 
-static void initVulkan(void) {
+
+
+
+
+static void createInstance(void) {
+    // optional data that may provide useful information to the driver
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
     
+    // mandatory data that tells Vulkan driver what global extensions and
+    //      validation layers to use
+    VkInstanceCreateInfo createInfo;
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    
+    // Vulkan is platform agnostic, so need extension to interface with window
+    //      system. Uses GLFW's built-in required extension finder.
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    
+    createInfo.enabledLayerCount = 0;
+    
+    // creates the instance, stored outside of stack frame
+    vkCreateInstance(&createInfo, NULL, &instance);
 }
+
+static void initVulkan(void) {
+    createInstance();
+    
+    // did not check if extensions are all supported
+}
+
+
+
+
+
 
 static void mainLoop(GLFWwindow *window) {
     // keeps the application running until window closed
@@ -36,7 +83,14 @@ static void mainLoop(GLFWwindow *window) {
     }
 }
 
+
+
+
+
 static void cleanup(GLFWwindow *window) {
+    // destroys the Vulkan instance
+    vkDestroyInstance(instance, NULL);
+    
     // deallocates window memory
     glfwDestroyWindow(window);
     
@@ -44,9 +98,15 @@ static void cleanup(GLFWwindow *window) {
     glfwTerminate();
 }
 
+
+
+
+
 void run(void) {
-    GLFWwindow* window = initWindow();
     initVulkan();
+    
+    GLFWwindow* window = initWindow();
+    
     mainLoop(window);
     cleanup(window);
 }
