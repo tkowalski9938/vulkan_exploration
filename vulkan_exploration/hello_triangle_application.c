be3c2613809ade3ccfc0a2d324ebb130d91d7419
chopin9938 <tkowalski9938@gmail.com>
2022-04-25 10:49:46 -0400

    Initial Commit

---
+++ vulkan_exploration/hello_triangle_application.c
@@ -0,0 +1,19 @@
+#include "hello_triangle_application.h"
+
+static void init_vulkan(void) {
+    
+}
+
+static void main_loop(void) {
+    
+}
+
+static void cleanup(void) {
+    
+}
+
+void run(void) {
+    init_vulkan();
+    main_loop();
+    cleanup();
+}
---
+++ vulkan_exploration/main.c
@@ -0,0 +1,8 @@
+#include "hello_triangle_application.h"
+
+#include <stdio.h>
+
+int main(void) {
+    fprintf(stderr, "hello world");
+    return 0;
+}
