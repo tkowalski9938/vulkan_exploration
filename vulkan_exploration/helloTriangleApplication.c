1caf6fc5b1ba798aceb559193ed64c6ac1de5e02
chopin9938 <tkowalski9938@gmail.com>
2022-04-26 10:13:30 -0400

    finds device

--- vulkan_exploration/helloTriangleApplication.c
+++ vulkan_exploration/helloTriangleApplication.c
@@ -6,14 +6,50 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 #include <stdbool.h>
 
 static GLFWwindow* window;
 static VkInstance instance;
+static VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
 
 static const uint32_t WIDTH = 800;
 static const uint32_t HEIGHT = 600;
+
+const char *validationLayers[] = {"VK_LAYER_KHRONOS_validation"};
+const int numValidationLayers = 1;
+
+// DISABLE during deployment
+const bool enableValidationLayers = true;
+
+// checks if all the requested layers are avaiable
+static bool checkValidationLayerSupport(void) {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, NULL);
+    
+    VkLayerProperties *availableLayers = malloc(sizeof(VkLayerProperties) *
+                                                layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers);
+    
+    
+    for (int i = 0; i < numValidationLayers; ++i) {
+        bool layerFound = false;
+        for (int k = 0; k < layerCount; ++k) {
+            if (strcmp(validationLayers[i], availableLayers[k].layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+        if (!layerFound) {
+            free(availableLayers);
+            return false;
+        }
+    }
+    free(availableLayers);
+    return true;
+}
 
 static void initWindow(void) {
     // initializes the GLFW library
@@ -34,15 +70,19 @@ static void createInstance(void) {
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
     
-    VkExtensionProperties *p_extensions = malloc(extensionCount * sizeof(VkExtensionProperties));
+    VkExtensionProperties *pExtensions = malloc(extensionCount * sizeof(VkExtensionProperties));
     
-    vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, p_extensions);
+    vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, pExtensions);
     
     printf("avaible extensions:\n");
     for (int i = 0; i < extensionCount; ++i) {
-        printf("\t%s\n", p_extensions[i * sizeof(VkExtensionProperties)].extensionName);
+        printf("\t%s\n", pExtensions[i * sizeof(VkExtensionProperties)].extensionName);
     }
-    free(p_extensions);
+    free(pExtensions);
+    
+    // checks if all validationLayers exist in availableLayers
+    assert((!enableValidationLayers || checkValidationLayerSupport()) &&
+           "validation layers requested, but not available");
     
     
     // optional data that may provide useful information to the driver
@@ -70,15 +110,55 @@ static void createInstance(void) {
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
     
-    createInfo.enabledLayerCount = 0;
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = numValidationLayers;
+        createInfo.ppEnabledLayerNames = validationLayers;
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
     
     // creates the instance, stored outside of stack frame. checks if successful
     assert((vkCreateInstance(&createInfo, NULL, &instance) == VK_SUCCESS) &&
            "failed to create instance");
 }
 
+static bool isDeviceSuitable(VkPhysicalDevice device) {
+    return true;
+}
+
+static void getPhysicalDeviceProperties(void) {
+    VkPhysicalDeviceProperties *properties = malloc(sizeof(VkPhysicalDeviceProperties));
+    
+    vkGetPhysicalDeviceProperties(physicalDevice, properties);
+    
+    printf("\n%s\n", properties->deviceName);
+    free(properties);
+}
+static void pickPhysicalDevice(void) {
+    
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
+    
+    assert(deviceCount > 0 && "failed to find GPUs with Vulkan support");
+    
+    VkPhysicalDevice *devices = malloc(deviceCount * sizeof(VkPhysicalDevice));
+    
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+    
+    for (int i = 0; i < deviceCount; ++i) {
+        if (isDeviceSuitable(devices[i])) {
+            physicalDevice = devices[i];
+            break;
+        }
+    }
+    free(devices);
+    assert(physicalDevice != VK_NULL_HANDLE && "failed to find suitable GPU");
+}
+
 static void initVulkan(void) {
     createInstance();
+    pickPhysicalDevice();
+    getPhysicalDeviceProperties();
 }
 
 static void mainLoop(void) {
