d101c6f4224f564d2099bdad38b06eb1caf79fd4
chopin9938 <tkowalski9938@gmail.com>
2022-04-27 08:49:46 -0400

    start over

--- vulkan_exploration/validation.h
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-
-extern const char *validationLayers[];
-
-extern const int numValidationLayers;
-
-extern const bool enableValidationLayers;
-
-bool checkValidationLayerSupport(void);
