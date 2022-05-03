7911fbadec6af6c87e9e971c216fa19b29cbd095
chopin9938 <tkowalski9938@gmail.com>
2022-05-02 22:14:29 -0400

    added shader stages to pipeline

--- vulkan_exploration/graphicsPipeline/graphicsPipeline.c
+++ vulkan_exploration/graphicsPipeline/graphicsPipeline.c
@@ -30,6 +30,26 @@ void createGraphicsPipeline(VkDevice *device) {
     
     VkShaderModule fragShaderModule = createShaderModule(device, fragShader);
     
+    
+    // adding the vert shader to respective position in graphics pipeline
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+    
+    // adding the frag shader to respective position in graphics pipeline
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+    
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+    
+    
+    
+    
     vkDestroyShaderModule(*device, vertShaderModule, NULL);
     vkDestroyShaderModule(*device, fragShaderModule, NULL);
     
